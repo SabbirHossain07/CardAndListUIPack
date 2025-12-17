91f66abf8e91e78b2bf111f5836beaa3d2172c23
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-12-17 14:14:21 +0600

    Initial Commit

---
+++ CardAndListUIPack/CardAndListUIPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAndListUIPackApp.swift
+//  CardAndListUIPack
+//
+//  Created by Sopnil Sohan on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CardAndListUIPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
